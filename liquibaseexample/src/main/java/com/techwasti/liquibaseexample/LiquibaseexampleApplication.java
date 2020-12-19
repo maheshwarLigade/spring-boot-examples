3abef5ddb74871705f0608c4c0170ac27820e513
maheshwarligade <maheshwar.ligade@gmail.com>
2020-12-19 16:03:20 +0530

    liquibase spring boot example

---
+++ liquibaseexample/src/main/java/com/techwasti/liquibaseexample/LiquibaseexampleApplication.java
@@ -0,0 +1,13 @@
+package com.techwasti.liquibaseexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseexampleApplication.class, args);
+	}
+
+}
