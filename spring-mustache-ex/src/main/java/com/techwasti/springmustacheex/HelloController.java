2a27c55ca1c84f8fb9ef5b0d3444c9b424571957
maheshwarligade <maheshwar.ligade@gmail.com>
2021-01-01 11:32:39 +0530

    Request param example changes

--- spring-mustache-ex/src/main/java/com/techwasti/springmustacheex/HelloController.java
+++ spring-mustache-ex/src/main/java/com/techwasti/springmustacheex/HelloController.java
@@ -1,8 +1,12 @@
 package com.techwasti.springmustacheex;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -15,5 +19,15 @@ public class HelloController {
         model.addAttribute("message", helloworld);
 
         return "hello";
+    }
+    // this is we have added to demonstrate the @RequestParam annotation.
+    @RequestMapping(path="/greeting", produces=MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public String greetUser(@RequestParam(defaultValue="GuestUser") String name,
+                              @RequestParam(required = false) String adult) {
+
+        var greet = "on".equals(adult) ? "Good morning" : "Hi";
+
+        return String.format("%s %s!", greet, name);
     }
 }
