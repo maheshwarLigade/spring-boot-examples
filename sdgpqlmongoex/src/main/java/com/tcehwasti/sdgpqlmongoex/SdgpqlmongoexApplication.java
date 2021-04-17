86213ec7b266354ad34c405c40003bd2469dcb6e
maheshwarligade <maheshwar.ligade@gmail.com>
2021-04-17 11:44:35 +0530

    fresh code example for mongodb graphql

---
+++ sdgpqlmongoex/src/main/java/com/tcehwasti/sdgpqlmongoex/SdgpqlmongoexApplication.java
@@ -0,0 +1,13 @@
+package com.tcehwasti.sdgpqlmongoex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdgpqlmongoexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdgpqlmongoexApplication.class, args);
+	}
+
+}
---
+++ sdgpqlmongoex/src/test/java/com/tcehwasti/sdgpqlmongoex/SdgpqlmongoexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcehwasti.sdgpqlmongoex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdgpqlmongoexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
